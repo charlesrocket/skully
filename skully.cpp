ba3c04472bae654347495d305c8164e934845f04
-k <slowdive@me.com>
2022-01-11 15:09:52 -0800

    Update `drawTime`

--- skully.cpp
+++ skully.cpp
@@ -17,27 +17,27 @@ void Skully::drawWatchFace(){
 }
 
 void Skully::drawTime(){
-    display.setFont(&GorgeousPixel28pt7b);
-    display.setCursor(137, 148);
+    display.setFont(&GorgeousPixel29pt7b);
+    display.setCursor(134, 147);
     String h = String(currentTime.Hour);
     String m = String(currentTime.Minute);
     if(currentTime.Hour < 10){
         display.print("0");
-        display.setCursor(166, 148);
+        display.setCursor(164, 147);
         display.print(String(h.substring(0,1)));
     } else {
         display.print(String(h.substring(0,1)));
-        display.setCursor(166, 148);
+        display.setCursor(164, 147);
         display.print(String(h.substring(1,2)));
     }
-    display.setCursor(137, 193);
+    display.setCursor(134, 193);
     if(currentTime.Minute < 10){
         display.print("0");
-        display.setCursor(166, 193);
+        display.setCursor(164, 193);
         display.print(String(m.substring(0,1)));
     } else {
         display.print(String(m.substring(0,1)));
-        display.setCursor(166, 193);
+        display.setCursor(164, 193);
         display.print(String(m.substring(1,2)));
     }
 }
