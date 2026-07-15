d1896528654b4f03e24e3597f369555b1f8380d0
-k <slowdive@me.com>
2026-07-14 20:22:21 -0400

    Switch to NYC

--- settings.h
+++ settings.h
@@ -2,15 +2,15 @@
 #define SETTINGS_H
 
 //Weather Settings
-#define CITY_ID "5368361" //Los Angeles https://openweathermap.org/current#cityid
-#define OPENWEATHERMAP_APIKEY "f058fe1cad2afe8e2ddc5d063a64cecb" //use your own API key :)
-#define OPENWEATHERMAP_URL "http://api.openweathermap.org/data/2.5/weather?id=" //open weather api
-#define TEMP_UNIT "metric" //metric = Celsius , imperial = Fahrenheit
+#define CITY_ID "5128581" // New York https://openweathermap.org/current#cityid
+#define OPENWEATHERMAP_APIKEY "f058fe1cad2afe8e2ddc5d063a64cecb" // use your own API key :)
+#define OPENWEATHERMAP_URL "http://api.openweathermap.org/data/2.5/weather?id=" // open weather api
+#define TEMP_UNIT "metric" // metric = Celsius , imperial = Fahrenheit
 #define TEMP_LANG "en"
-#define WEATHER_UPDATE_INTERVAL 30 //must be greater than 5, measured in minutes
+#define WEATHER_UPDATE_INTERVAL 30 // must be greater than 5, measured in minutes
 //NTP Settings
 #define NTP_SERVER "pool.ntp.org"
-#define GMT_OFFSET_SEC 3600 * -7 //Los Angeles is UTC -7
+#define GMT_OFFSET_SEC 3600 * -4 // New York is UTC -4
 
 watchySettings settings{
     .cityID = CITY_ID,
